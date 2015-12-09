d03897c314616672b94d7133c2d64656c0bf8b3b
v_chenxinmin <v_chenxinmin@3aa6a452-0efb-431a-b1a8-75ed96c0717c>
2015-12-09 10:10:10 +0000

    会员客群计算nrpt2迁移到dw上

--- segment/segment-module/src/com/wanda/ccs/member/segment/SegmentConstants.java
+++ segment/segment-module/src/com/wanda/ccs/member/segment/SegmentConstants.java
@@ -6,6 +6,7 @@ public class SegmentConstants {
 	public static class Schemas {
 		public final static String RPT2 = "CCS_NRPT2"; //报表库schema名称'
 		public final static String MBRODS = "CCS_MBR_DW"; //会员应用数据库schema名称
+		public final static String CCSDW = "CCS_DW"; //报表库schema名称'
 	}
 	
 	public final static String MARTERIAL_TYPE =  "03";
